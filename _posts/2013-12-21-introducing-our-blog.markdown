28917ff26d1043ba4d3c78f0eb9dfbe518a5485e
Victoria Bondarchuk <me@victoriabondarchuk.com>
2013-12-21 21:45:40 +0900

    new design

---
+++ _posts/2013-12-21-introducing-our-blog.markdown
@@ -0,0 +1,10 @@
+---
+layout: post
+title:  "Introducing our Blog"
+date:   2013-12-21 21:18:19
+---
+
+About us
+
+[Victoria Bondarchuk]:      http://victoriabondarchuk.com
+[Yeaeun Kwon]:      http://YeaeunKwon.com
---
+++ js/script.js
@@ -0,0 +1,35 @@
+var colors = [
+  '#ED5565',
+  '#DA4453',
+  '#FC6E51',
+  '#E9573F',
+  '#FFCE54',
+  '#F6BB42',
+  '#A0D468',
+  '#8CC152',
+  '#48CFAD',
+  '#37BC9B',
+  '#4FC1E9',
+  '#3BAFDA',
+  '#5D9CEC',
+  '#4A89DC',
+  '#AC92EC',
+  '#967ADC',
+  '#EC87C0',
+  '#D770AD',
+  '#CCD1D9',
+  '#AAB2BD',
+  '#656D78',
+  '#434A54'
+]
+
+// Set a different color on each reload
+var col = colors[Math.floor(Math.random() * colors.length)];
+var el = document.querySelector('span.blog_image');
+el.style.backgroundColor = col; 
+
+/*
+var links = document.querySelectorAll('a');
+for(var i = 0; i < links.length; i++)
+  links[i].style.color = col;
+*/
